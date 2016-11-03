76b143ab6d84d505ac794b448eea4e4dfb7166ff
Walter <walterayalae43@gmail.com>
2016-11-03 10:09:59 -0500

    Created server, added bootstrap, started front end development, added experience and projects

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'), 
+    http = require('http'), 
+    path = require('path');
+    
+
+var app = express();
+
+
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.static(path.join(__dirname, 'public')));
+
+// Render our home page with all blog posts
+app.get('/', function(request, response) {
+        
+            response.render('index')
+        
+});
+
+// // Render a form to enter a new post
+// app.get('/new', function(request, response) {
+//     response.render('new', {});
+// });
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
