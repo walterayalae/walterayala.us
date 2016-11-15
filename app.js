974b2264c3eecedae121690abfa503858e4d0505
Walter <walterayalae43@gmail.com>
2016-11-14 21:32:49 -0600

    added html to app.js

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ var app = express();
 
     app.set('port', process.env.PORT || 8080);
     app.set('views', __dirname + '/views');
-    app.set('view engine', 'html');
     app.use(express.static(path.join(__dirname, 'public')));
 
 // Render our home page with all blog posts
 app.get('/', function(request, response) {
         
-            response.render('index')
+            response.render('index.html')
         
 });
 
